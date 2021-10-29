52099f993c53cea9a08f7d7aa1cc066f59fa0bfc
bybbsy <79372250+bybbsy@users.noreply.github.com>
2021-10-29 13:32:58 +0300

    Added configs in vue.config.js

--- vue-app/vue.config.js
+++ vue-app/vue.config.js
@@ -1,3 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                return [{
+                    title: 'Not-vue-app',
+                }]
+            })
+    }
 }
