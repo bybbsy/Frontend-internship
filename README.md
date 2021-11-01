4bfc6a0d31ea72d7becf8ea78335fdc5998a1edf
bybbsy <79372250+bybbsy@users.noreply.github.com>
2021-11-01 12:39:32 +0300

    Added vue.config.js

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Frontend-internship
 
-### :rocket: Basics -> iterations 1-7
-### :rocket: Vue -> iterations 8-13
+#### :rocket: Basics -> iterations 1-7
+#### :rocket: Vue -> iterations 8-13
--- vue-app/vue.config.js
+++ vue-app/vue.config.js
@@ -1,13 +1,15 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
     chainWebpack: config => {
-        config
-            .plugin('html')
-            .tap(args => {
-                return [{
-                    title: 'Not-vue-app',
-                }]
-            })
+      config
+        .plugin('html')
+        .tap(args => {
+            args[0].title = 'Not a vue app'
+           //  args[0] contains the plugin's options object
+          // change it to what you need it to be.
+          return args
+        })
     }
-}
+  }
