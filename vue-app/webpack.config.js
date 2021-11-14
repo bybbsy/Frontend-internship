823653cd282f3b3e9edf1a71a2e11f87d2332ce7
Kirill <jimmy_boi@mail.ru>
2021-11-14 23:42:36 +0300

    Webpack config has been added

---
+++ vue-app/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/main.js',
+    output: {
+        filename: '[name].[contenthash].js',
+        path : path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new CleanWebpackPlugin()
+    ]
+}
