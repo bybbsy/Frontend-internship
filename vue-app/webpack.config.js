8b6218b597d7c92a5f4784686f90a11168facaed
bybbsy <jimmy_boi@mail.ru>
2021-11-23 08:01:51 +0300

    Added extra line

--- vue-app/webpack.config.js
+++ vue-app/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
 module.exports = {
     mode: 'production',
     entry: './src/main.js',
