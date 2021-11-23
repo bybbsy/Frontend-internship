c9337d7bfd3cc981176062cd67cc953c56e184c6
bybbsy <jimmy_boi@mail.ru>
2021-11-23 07:56:36 +0300

    Added a products page and a product page

---
+++ vue-app/src/views/Product.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="product-block">
+      <div class="product-wrapper">
+          <h5>Product information</h5>
+          <div class="product-info">
+
+              <div class="product-title">{{ title }}</div>
+              <div class="product-pic">
+                  <img :src="url" alt="">
+              </div>
+
+              <div class="product-desc">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo sed minus in ut facere cupiditate, dolor perferendis fugiat velit nesciunt. Repellat, hic quis nobis minus ipsam quam aut fugit vel!
+                  Optio animi corporis placeat. Obcaecati at minima, voluptas cum est iusto aliquam aspernatur harum perspiciatis aut, qui iure ab suscipit tenetur! Saepe provident eaque voluptas enim cupiditate ut et deleniti.
+                  Quaerat tempore asperiores voluptates sit dolore fugit illo! Aliquam, quisquam ipsa, molestiae quibusdam dolorum, labore officiis quaerat perspiciatis consequatur necessitatibus harum minus vel natus at assumenda recusandae incidunt maxime eum.</p>
+              </div>
+
+              <div class="buttons">
+                  <button type="button">Add to wishlist</button>
+                  <button type="button">Purchase</button>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            title: null,
+            url: null
+        }
+    },
+    beforeCreate() {
+        return fetch(`https://jsonplaceholder.typicode.com/photos/${this.$route.params.id}`)
+          .then(response => response.json())
+          .then(json => {
+              this.title = json.title
+              this.url = json.url
+          })
+    }
+}
+</script>
+
+<style>
+.product-block {
+    height: 100%;
+}
+
+.product-info {
+    display: grid; 
+    margin: 0 25px;
+    grid-auto-columns: minmax(350px, 450px) 1fr;
+    grid-auto-rows: 50px minmax(250px, 300px) minmax(150px, 200px);
+    background-color: rgba(255, 255, 255, 0.842);
+    border-radius: 15px;
+}
+
+.product-title {
+    grid-column: 1 / 2;
+    grid-row: 1 / 1;
+    text-align: center;
+    font-weight: bold;
+    font-size: 1.2em;
+}
+
+.product-pic {
+    grid-column: 1 / 1;
+    grid-row: 2 / 2;
+}
+
+.product-pic img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+}
+
+.product-desc {
+    grid-column: 2 / 3;
+    grid-row: 2 / 3;
+    font-size: 1.1em;
+}
+
+.buttons {
+    display: flex;
+    grid-column: 2 / 3;
+    grid-row: 3 / 4;
+    justify-content: flex-end;
+    align-items: flex-start;
+}
+
+.buttons button {
+    margin: 0 15px;
+    background-color: tomato;
+    outline: none;
+    font-size: 1.1em;
+    color: #fff;
+    border: unset;
+    cursor: pointer;
+}
+
+.buttons button:hover {
+    box-shadow: 0px 0px 100px 100px rgba(17, 17, 17, 0.205) inset;;
+}
+</style>
--- vue-app/src/views/Products.vue
+++ vue-app/src/views/Products.vue
@@ -1,18 +1,80 @@
 <template>
-  <div>
-      <span style="font-size: 12em; color: red">asdasdas</span>
+  <div class="products-block">
+      <aside class="sub-header">
+        <h5>Products list</h5>
+        <div class="info-block">
+          <p>Fake data provided by <a href="https://jsonplaceholder.typicode.com/" target="_blank">JSON Placholed</a> </p> 
+        </div>
+      </aside>
+
+      <div class="cards-block" v-if="receivedData">
+          <Card v-for="(card, index) in receivedData"
+          :key="index"
+          :card="card"
+        />
+      </div>
+      <div v-else>
+        <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+      </div>
   </div>
 </template>
 
 <script>
+import Card from "../components/Card.vue";
 
 export default {
+
   name: 'Products',
+  data: function() {
+    return {
+      receivedData: null
+    }
+  },
+  beforeCreate() {
+    let data = fetch('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=10')
+          .then(response => response.json())
+          .then(json => this.receivedData = json)
+
+    console.log(data)
+  },
   components: {
+    Card
   }
 }
 </script>
 
 <style>
+.products-block {
+  height: 100%;
+}
+.sub-header {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background-color: #fff;
+  border-radius: 0px 0px 50px 50px / 0px 4px 50px 50px;
+}
 
+.sub-header h5 {
+  font-size: 1.2em;
+  text-align: center;
+}
+
+.info-block {
+  text-align: center;
+}
+
+.info-block p {
+  margin: 2px 0;
+}
+
+.info-block p a {
+  color: tomato;
+}
+
+.cards-block {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+}
 </style>
