13a15fcbf70cf1a2c03004372f79f5253a131f52
bybbsy <jimmy_boi@mail.ru>
2021-12-13 01:14:20 +0300

    Changed routes

--- vuex-app/src/router/index.js
+++ vuex-app/src/router/index.js
@@ -8,12 +8,32 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/bom',
+    name: 'bom',
+    meta: { layout: 'empty'},
+    component: () => import('../views/NextPage.vue')
+  },
+  {
+      path: '/products',
+      component: () => import('../layouts/MainLayout.vue'),
+      children: [
+          {
+              path: '',
+              name: 'products',
+              component: () => import('../views/Products.vue')
+          },
+          {
+              path: 'id=:id',
+              name: 'product',
+              component: () => import('../views/Product.vue')
+          }
+      ]
+  },
+  {
+      path: '*',
+      name: 'no-page',
+      meta: { layout: 'empty' },
+      component: () => import('../views/ErrorPage.vue')
   }
 ]
 
