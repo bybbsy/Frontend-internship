1da16bdfdad0b37c81ec603e6943d7a1e0556f44
bybbsy <jimmy_boi@mail.ru>
2021-12-13 03:29:44 +0300

    Added basic router

--- vuex-app/src/router/index.js
+++ vuex-app/src/router/index.js
@@ -1,40 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
+    meta: { layout: 'main' },
     component: Home
   },
-  {
-    path: '/bom',
-    name: 'bom',
-    meta: { layout: 'empty'},
-    component: () => import('../views/NextPage.vue')
-  },
-  {
-      path: '/products',
-      component: () => import('../layouts/MainLayout.vue'),
-      children: [
-          {
-              path: '',
-              name: 'products',
-              component: () => import('../views/Products.vue')
-          },
-          {
-              path: 'id=:id',
-              name: 'product',
-              component: () => import('../views/Product.vue')
-          }
-      ]
-  },
-  {
-      path: '*',
-      name: 'no-page',
-      meta: { layout: 'empty' },
-      component: () => import('../views/ErrorPage.vue')
-  }
+  // {
+  //   path: '/bom',
+  //   name: 'bom',
+  //   meta: { layout: 'empty'},
+  //   component: () => import('../views/NextPage.vue')
+  // },
+  // {
+  //     path: '/products',
+  //     component: () => import('../layouts/MainLayout.vue'),
+  //     children: [
+  //         {
+  //             path: '',
+  //             name: 'products',
+  //             component: () => import('../views/Products.vue')
+  //         },
+  //         {
+  //             path: 'id=:id',
+  //             name: 'product',
+  //             component: () => import('../views/Product.vue')
+  //         }
+  //     ]
+  // },
+  // {
+  //     path: '*',
+  //     name: 'no-page',
+  //     meta: { layout: 'empty' },
+  //     component: () => import('../views/ErrorPage.vue')
+  // }
 ]
 
 const router = createRouter({
