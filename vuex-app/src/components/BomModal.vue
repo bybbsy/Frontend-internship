7896c6134d4c53ff8e6fe727e98acfcb2e92f32c
bybbsy <jimmy_boi@mail.ru>
2021-12-13 03:33:04 +0300

    Added main components

---
+++ vuex-app/src/components/BomModal.vue
@@ -0,0 +1,123 @@
+<template>
+  <div>
+        <div class="row__current-url">
+            <p>Current URL: <router-link :to="{ name: 'home' }" class="current-url__value">{{ currentUrl }}</router-link></p>
+            <!-- <p><router-link :to="{ name: 'bom' }" class="current-url__next">Click here to achieve forward link! 🚀🚀🚀</router-link></p> -->
+        </div>
+        <div class="row__manage-history">
+            <div class="manage-back" @click="goBack">
+                <i class="fas fa-backward"></i>
+                <div class="hint">Go to the previous page</div>
+            </div>
+            <div class="manage-forward" @click="goForward">
+                <i class="fas fa-forward"></i>
+                <div class="hint">Go the the next page</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'bom-modal',
+    props: {
+        modalData: Object
+    },
+    data: function() {
+        return {
+            currentUrl: null
+        }
+    },
+    created: function() {
+        this.currentUrl = location.href
+    },
+    methods: {
+        goForward: function() {
+            this.$router.go(1)
+        },
+        goBack: function() {
+            this.$router.go(-1)
+        }
+    }
+}
+</script>
+
+<style>
+.modal-bom {
+      position: absolute;
+      left: 50%;
+      top: 25%;
+      transform: translate(-50%, -25%);
+      width: 500px;
+      height: 220px;
+      border-radius: 25px;
+      border: 1px dashed tomato;
+      background-color: #fff;
+      display: grid;
+      grid-template-rows: repeat(2, 100px);
+      z-index: 1;
+  }
+
+  .modal-bom.hidden {
+      display: none;
+  }
+
+  .row__current-url,
+  .row__manage-history {
+      display: grid;
+      align-items: center;
+      padding: 0 10px;
+  }
+
+  .current-url__value {
+      display: inline-flex;
+      width: 360px;
+      text-overflow: ellipsis;
+      white-space: nowrap; /* paragraph to one line */
+      overflow:hidden; /* older browsers */
+  }
+
+  .row__manage-history {
+      grid-template-columns: repeat(2, 1fr);
+      justify-items: center;
+  }
+
+  .manage-back,
+  .manage-reload,
+  .manage-forward {
+      position: relative;
+      cursor: pointer;
+  }
+
+  .manage-back .fas,
+  .manage-reload .fas,
+  .manage-forward .fas {
+      font-size: 2em;
+      color: tomato;
+  }
+
+  .manage-back:hover .hint,
+  .manage-reload:hover .hint,
+  .manage-forward:hover .hint {
+      visibility: visible;
+      opacity: 1;
+      height: fit-content;
+      transition: 0.2s all ease 0.8s;
+  }
+
+  .hint {
+      opacity: 0;
+      position: absolute;
+      visibility: hidden;
+      width: 100px;
+      left: 0;
+      bottom: 0;
+      margin-bottom: -100%;
+      height: 0;
+      background-color: #fff;
+      box-shadow: 0px 0px 5px 2px rgba(17, 17, 17, 0.69);
+      font-size: 0.8em;
+      padding: 2px;
+  }
+
+</style>
---
+++ vuex-app/src/components/ModalPopup.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="pop-up" :class="{ [onSignUp.type]: onSignUp.type }">
+        <div class="pop-up__wrapper">
+            <p>{{ onSignUp.message }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'pop-up',
+    props: {
+        onSignUp: Object
+    }
+}
+</script>
+
+<style>
+.pop-up:first-child {
+      margin-top: 0;
+  }
+
+  .pop-up {
+      width: 250px;
+      max-width: 255px;
+      min-height: 2em;
+      margin-top: 5px;
+      margin-bottom: 5px;
+      transition: 0.2s all ease;
+  }
+
+  .pop-up__wrapper {
+      position: relative;
+      display: flex;
+      width: 100%;
+      height: 100%;
+      background-color: white;
+      align-items: center;
+      border-radius: 5px;
+  }
+  
+  .pop-up__wrapper::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 10px;
+      height: 100%;
+      border-bottom-left-radius: 5px;
+      border-top-left-radius: 5px;
+  }
+
+  .pop-up.declined .pop-up__wrapper::before {
+      background-color: red;
+  }
+
+  .pop-up.successful .pop-up__wrapper::before {
+      background-color: yellowgreen;
+  }
+
+  .pop-up__wrapper p {
+      display: flex;
+      margin: 2px 0 2px 15px;
+  }
+</style>
