49bf3f9e2070244b1b43ae9b1225dd4644d68562
Kirill <jimmy_boi@mail.ru>
2021-12-19 15:36:28 +0300

    It uses comp api now

---
+++ vuex-app/src/components/TheAccordion.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="product-accordion" :class="{ open: isOpen }">
+    <div class="accordion-header" @click="toggleAccordion">
+      <slot name='accordion-title'/>
+      <i class="fas fa-chevron-down" :class="{ close: isOpen }"></i>
+    </div>
+    <div v-show="isOpen" class="content">
+      <slot name="content"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import {ref} from "vue";
+export default {
+  name: 'the-accordion',
+  setup() {
+    let isOpen = ref(false);
+
+    function toggleAccordion() {
+      isOpen.value = !isOpen.value;
+    }
+
+    return {
+      isOpen,
+      toggleAccordion
+    }
+  }
+}
+</script>
+
+<style>
+.accordion-header {
+  cursor: pointer;
+  display: flex;
+  width: fit-content;
+}
+.product-accordion {
+  margin: 15px 25px 5px;
+}
+
+.fas.fa-chevron-down {
+  transition: 0.5s all ease;
+}
+
+.fas.fa-chevron-down.close {
+  color: tomato;
+  transform: rotateX(180deg);
+}
+</style>
