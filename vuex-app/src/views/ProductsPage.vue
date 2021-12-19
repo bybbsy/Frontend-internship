324a6f44b21dfdc8b4045024712bcd65f2f89501
Kirill <jimmy_boi@mail.ru>
2021-12-19 13:40:33 +0300

    Added wrapper page for products

---
+++ vuex-app/src/views/ProductsPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="products-page__wrapper">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "products-page"
+}
+</script>
+
+<style scoped>
+
+</style>
