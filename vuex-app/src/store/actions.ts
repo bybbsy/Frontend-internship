831ed49f5c1abff9297dc0cdd9c0cc009fa3274e
bybbsy <jimmy_boi@mail.ru>
2021-12-29 16:43:45 +0300

    Added action types, context and actions

--- vuex-app/src/store/actions.ts
+++ vuex-app/src/store/actions.ts
@@ -1,18 +1,28 @@
-import { State } from "./state";
-import { MutationType } from './mutations'
-import { ActionTree } from "vuex";
+import { State, state, Albums } from "./state";
+import { ActionContext, ActionTree, Commit } from "vuex";
+import { Mutations, MutationTypes } from "./mutations";
 
-export const actions: ActionTree<State, State> = {
-    async fetchProducts({commit}) {
-        return fetch('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=10')
+export enum ActionTypes {
+    fetchProducts = 'fetchProducts',
+    toggleModal = 'toggleModal'
+}
+
+type AugmentedActionContext = {
+    commit<K extends keyof Mutations>(key: K, payload: Parameters<Mutations[K]>[1]): ReturnType<Mutations[K]>;
+} & Omit<ActionContext<State, State>, 'commit'>
+
+export interface Actions {
+    [ActionTypes.fetchProducts]({commit}: AugmentedActionContext): void
+    [ActionTypes.toggleModal](): void
+}
+
+export const actions: ActionTree<State, State> & Actions = {
+    async [ActionTypes.fetchProducts]({commit}) {
+        return fetch('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=100')
             .then(response => response.json())
-            .then(data => commit(MutationType.setProducts, data))
+            .then(data => commit(MutationTypes.setProducts, data))
             .catch(e => console.log('error: ', e))
+    },
+    [ActionTypes.toggleModal]() {
     }
-    // async fetchProducts({commit}) {
-    //     return fetch('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=10')
-    //         .then(response => response.json())
-    //         .then(data => commit(MutationType.setProducts, data))
-    //         .catch(e => console.log('error: ', e))
-    // }
 }
