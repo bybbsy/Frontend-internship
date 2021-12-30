f1927e0d139bd9d3ecafb90c0463e4e6317ca3d0
bybbsy <jimmy_boi@mail.ru>
2021-12-30 18:20:39 +0300

    Added mutations

--- vuex-app/src/store/mutations.ts
+++ vuex-app/src/store/mutations.ts
@@ -1,54 +1,27 @@
-import {State, ModalData, Albums} from "./state";
-import {MutationTree} from 'vuex'
+import { State, ModalData, Albums } from "./state";
+import { MutationTree } from 'vuex'
 
-export enum MutationType {
+export enum MutationTypes {
     toggleMenu = 'TOGGLE_MENU',
-    toggleModal = 'TOGGLE_MODAL',
     setModalData = 'SET_MODAL_DATA',
-    closeModal = 'CLOSE_MODAL',
     setProducts = 'SET_PRODUCTS'
 }
 
 export type Mutations = {
-    [MutationType.toggleMenu](state: State): void,
-    [MutationType.toggleModal](state: State, event: any): void,
-    [MutationType.setModalData](state: State, payload: ModalData): void,
-    [MutationType.closeModal](state: State): void,
-    [MutationType.setProducts](state: State, products: Albums[]): void
+    [MutationTypes.toggleMenu](state: State): void,
+    [MutationTypes.setModalData](state: State, payload: ModalData): void,
+    [MutationTypes.setProducts](state: State, products: Albums[]): void
 }
 
 export const mutations: MutationTree<State> & Mutations = {
-    [MutationType.toggleMenu](state) {
+    [MutationTypes.toggleMenu](state) {
         state.showMenu = !state.showMenu
     },
-    [MutationType.toggleModal](state, event) {
-        // If nav menu is open calls toggleMenu and then opens a modal
-        if (state.showMenu) {
-            this.TOGGLE_MENU;
-        }
-
-        state.modalData.showModal = !state.modalData.showModal;
-        // Length of the class to receive the last word
-        // which displays the name of link and the name of modal window to show
-        const len = event.target.classList.length;
-        // Puts the name of link in the name of modal to show
-        state.modalData.name = event.target.classList[len - 1];
-    },
-    [MutationType.setModalData](state, payload) {
+    [MutationTypes.setModalData](state, payload) {
         state.modalData = payload;
     },
-    [MutationType.closeModal](state) {
-        this.SET_MODAL_DATA(state, 
-            {name: '', 
-            showModal: false
-        });
-
-        // commit('setModalData', {
-        //     name: '',
-        //     showModal: false
-        // })
-    },
-    [MutationType.setProducts](state, products) {
+    [MutationTypes.setProducts](state, products) {
         state.receivedProducts = products
     }
 }
+
