9aab2714ce0a8b517fe60192b524d82df38b9a94
bybbsy <jimmy_boi@mail.ru>
2021-12-30 18:22:22 +0300

    Removed injection key

--- vuex-app/src/main.ts
+++ vuex-app/src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import {store, key} from './store'
+import {store} from './store'
 
 createApp(App)
-.use(store, key)
+.use(store)
 .use(router)
 .mount('#app')
