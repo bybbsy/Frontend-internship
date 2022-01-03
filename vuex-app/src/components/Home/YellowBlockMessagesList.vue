fee2e64d92b8fffa9e1eee21cf51491404807240
bybbsy <jimmy_boi@mail.ru>
2022-01-03 19:06:07 +0300

    Added ts

--- vuex-app/src/components/Home/YellowBlockMessagesList.vue
+++ vuex-app/src/components/Home/YellowBlockMessagesList.vue
@@ -1,79 +1,79 @@
 <template>
   <ul class="messages-list">
-      <YellowBlockMessage
+    <YellowBlockMessage
         v-for="(message, index) in filteredMessages"
         :key="index"
         :date="message.date"
         :author="message.author"
         :content="message.content"
         :class="{ spam: message.spam }"
-        />
+    />
   </ul>
 </template>
 
-<script>
-import { computed } from '@vue/reactivity';
+<script lang="ts">
+import {computed, defineComponent} from 'vue';
 import YellowBlockMessage from './YellowBlockMessage.vue'
 
-export default {
-    name: 'yellow-block-messages-list',
-    setup() {
-      const filterKeys = ['message', 'subscribed', 'notifications'];
-      const messages = [
-                {
-                    date: '12.02.21',
-                    author: 'John Doe',
-                    content: 'This message was sent to you because you subscribed on our daily e-mail notifications. Just keep you posted, bud.'
-                },
-                {
-                    date: '12.08.21',
-                    author: 'John Doe',
-                    content: 'This message was sent to you because you subscribed on our daily e-mail notifications. Just keep you posted, bud.'
-                },
-                {
-                    date: '12.08.21',
-                    author: 'Jeff La Quero',
-                    content: 'Hey. bud. Wanna hang out tis weekend?'
-                },
-                {
-                    date: '12.02.21',
-                    author: 'John Doe',
-                    content: 'This message was sent to you because you subscribed on our daily e-mail notifications. Just keep you posted, bud.'
-                }
-            ];
+export default defineComponent({
+  name: 'yellow-block-messages-list',
+  setup() {
+    const filterKeys: string[] = ['message', 'subscribed', 'notifications'];
 
-      function filterMessage(msg) {
-        for(let key in filterKeys) {
-          if(msg.includes(filterKeys[key])) {
-            return true
-          }
-        }
-        return false
-      }
-
-      let filteredMessages = computed(() => {
-        return messages.map(message => {
-          let spamMsg = filterMessage(message.content);
-
-          if(spamMsg) {
-            message.spam = true
-          }
-          else {
-            message.spam = false
-          }
-          return message
-        })
-      })
-
-      return {
-        messages,
-        filteredMessages
-      }
-    },
-    components: {
-        YellowBlockMessage
+    interface message {
+      date: string | Date,
+      author: string,
+      content: string
     }
-}
+
+    const messages: message[] = [
+      {
+        date: '12.02.21',
+        author: 'John Doe',
+        content: 'This message was sent to you because you subscribed on our daily e-mail notifications. Just keep you posted, bud.'
+      },
+      {
+        date: '12.08.21',
+        author: 'John Doe',
+        content: 'This message was sent to you because you subscribed on our daily e-mail notifications. Just keep you posted, bud.'
+      },
+      {
+        date: '12.08.21',
+        author: 'Jeff La Quero',
+        content: 'Hey. bud. Wanna hang out tis weekend?'
+      },
+      {
+        date: '12.02.21',
+        author: 'John Doe',
+        content: 'This message was sent to you because you subscribed on our daily e-mail notifications. Just keep you posted, bud.'
+      }
+    ];
+
+    function filterMessage(msg: string) {
+      for (let key in filterKeys) {
+        if (msg.includes(filterKeys[key])) {
+          return true
+        }
+      }
+      return false
+    }
+
+    let filteredMessages = computed(() => {
+      return messages.map(message => {
+        let spam = filterMessage(message.content);
+        return {...message, spam};
+      })
+    })
+
+    return {
+      messages,
+      filteredMessages
+    }
+  },
+  components: {
+    YellowBlockMessage
+  }
+})
 </script>
 
 <style>
