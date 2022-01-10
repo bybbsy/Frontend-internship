8df97ef2b3aa626c887a78fddaef3cf4bf114384
bybbsy <jimmy_boi@mail.ru>
2022-01-10 16:12:52 +0300

    Added initial actions for moduleNavMenu

---
+++ vuex-app/src/store/navMenu/actions.ts
@@ -0,0 +1,17 @@
+import { ActionContext, ActionTree } from "vuex";
+import { MutationTypes } from "./mutations";
+import { State } from "./state";
+
+export enum ActionTypes {
+    toggleMenu = "toggleMenu"
+}
+
+export interface Actions {
+    [ActionTypes.toggleMenu]({commit}: ActionContext<State, State>): void
+}
+
+export const actions: ActionTree<State, State> & Actions = {
+    [ActionTypes.toggleMenu]({commit}) {
+        commit(MutationTypes.toggleMenuState)
+    }
+}
