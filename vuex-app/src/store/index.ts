f4e7e4985a3be21e38c2cf302108dd913a8e60af
bybbsy <jimmy_boi@mail.ru>
2022-01-11 14:32:28 +0300

    Store now consists of 3 separate modules
    Changes:
     - Implemented Modals, navMenu and Products modules;
     - Modals manages state of modal windows;
     - navMenu manages state of navigation menu on mobile devices;
     - Products manages state of received products for Products page;
     - Inner logic of store management was removed.

--- vuex-app/src/store/index.ts
+++ vuex-app/src/store/index.ts
@@ -1,18 +1,32 @@
 import {InjectionKey} from 'vue';
-import {State, state} from './state'
-import {mutations} from './mutations';
-import {actions} from './actions';
-import {getters} from './getters';
 import {createStore, Store, useStore as baseUseStore} from 'vuex'
 
-// Injection key for implementing in components
-export const key: InjectionKey<Store<State>> = Symbol();
+// Importing state of each module
+import { State as modalsState} from './Modals/state';
+import { State as menuState } from './navMenu/state';
+import { State as ProductsState } from './Products/state';
 
-export const store = createStore<State>({
-    state,
-    mutations,
-    actions,
-    getters
+// Importing modules
+import { moduleModals } from './Modals/index'
+import { moduleNavMenu } from './navMenu/index';
+import { moduleProducts } from './Products/index'
+
+// Injection key for implementing in components
+export const key: InjectionKey<Store<RootState>> = Symbol();
+
+// Register general state as RootState
+export type RootState = {
+    menu: menuState
+    modals: modalsState,
+    products: ProductsState
+}
+
+export const store = createStore({
+    modules: {
+        moduleNavMenu,
+        moduleModals,
+        moduleProducts
+    }
 })
 
 export function useStore() {
