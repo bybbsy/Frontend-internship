e3b36107bff11f210d49daa522af47f6e224ad25
bybbsy <jimmy_boi@mail.ru>
2022-01-11 14:28:37 +0300

    added navigation menu module

--- vuex-app/src/store/navMenu/getters.ts
+++ vuex-app/src/store/navMenu/getters.ts
@@ -1,11 +1,12 @@
 import { GetterTree } from "vuex";
 import { State } from "./state";
+import { RootState } from "@/store/index";
 
 export type Getters = {
     getShowMenu(state: State): boolean
 }
 
-export const getters: GetterTree<State, State> & Getters = {
+export const getters: GetterTree<State, RootState> & Getters = {
     getShowMenu(state) {
         return state.showMenu
     }
--- vuex-app/src/store/navMenu/index.ts
+++ vuex-app/src/store/navMenu/index.ts
@@ -1,10 +1,12 @@
-import { state } from "./state";
+import { State, state } from "./state";
+import { RootState } from "../index";
 import { mutations } from './mutations';
 import { actions } from './actions';
 import { getters } from './getters';
+import { Module } from "vuex";
 
 
-export const moduleNavMenu = {
+export const moduleNavMenu: Module<State, RootState> = {
     state,
     mutations,
     actions,
